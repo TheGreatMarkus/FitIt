65cc25fb292f40bbf2e11a00aed270c6498f3cf4
Cristian Aldea <aldea.cristian@outlook.com>
2020-03-21 16:27:50 -0400

    Install lombok and convert models

--- app/src/main/java/com/ui/fitit/models/Event.java
+++ app/src/main/java/com/ui/fitit/models/Event.java
@@ -1,50 +1,16 @@
 package com.ui.fitit.models;
 
-import java.sql.Time;
+import java.time.LocalTime;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Event {
-    private Time start;
-    private Time end;
+    private LocalTime start;
+    private LocalTime end;
     private List<Session> sessionList;
     private String place;
-
-    public Event(Time start, Time end, List<Session> sessionList, String place) {
-        this.start = start;
-        this.end = end;
-        this.sessionList = sessionList;
-        this.place = place;
-    }
-
-    public Time getStart() {
-        return start;
-    }
-
-    public void setStart(Time start) {
-        this.start = start;
-    }
-
-    public Time getEnd() {
-        return end;
-    }
-
-    public void setEnd(Time end) {
-        this.end = end;
-    }
-
-    public List<Session> getSessionList() {
-        return sessionList;
-    }
-
-    public void setSessionList(List<Session> sessionList) {
-        this.sessionList = sessionList;
-    }
-
-    public String getPlace() {
-        return place;
-    }
-
-    public void setPlace(String place) {
-        this.place = place;
-    }
 }
--- app/src/main/java/com/ui/fitit/models/Session.java
+++ app/src/main/java/com/ui/fitit/models/Session.java
@@ -2,28 +2,12 @@ package com.ui.fitit.models;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Session {
     private LocalDate date;
     private boolean attended;
-
-    public Session(LocalDate date, boolean attended) {
-        this.date = date;
-        this.attended = attended;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public boolean isAttended() {
-        return attended;
-    }
-
-    public void setAttended(boolean attended) {
-        this.attended = attended;
-    }
 }
