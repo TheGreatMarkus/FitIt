d8e29394830b30b5e0b3c436ee7c44c09663c488
ferdousara-parvin <parvin.ferdousara@gmail.com>
2020-04-05 12:54:48 -0400

    #1: started creating the schedule activity page

---
+++ app/src/main/java/com/ui/fitit/ui/ScheduleActivity.java
@@ -0,0 +1,15 @@
+package com.ui.fitit.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ui.fitit.R;
+
+public class ScheduleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.schedule_activity);
+    }
+}
