4937976575e9ccfb04c6ce42a3182a5ef8761ff9
ferdousara-parvin <parvin.ferdousara@gmail.com>
2020-04-07 21:30:05 -0400

    #1: formatting UI and created adapter

---
+++ app/src/main/java/com/ui/fitit/Adapters/EventAdapter.java
@@ -0,0 +1,68 @@
+package com.ui.fitit.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.ui.fitit.R;
+import com.ui.fitit.data.model.Event;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+
+public class EventAdapter extends ArrayAdapter<Event> {
+    private Context context;
+    private List<Event> events;
+
+    // UI Elements
+    private TextView title;
+    private TextView time;
+    private TextView location;
+    private TextView day;
+    private TextView month;
+
+    public EventAdapter(@NonNull Context context, int textViewResourceId, @NonNull List<Event> events) {
+        super(context, textViewResourceId, events);
+        this.events = events;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View viewToDisplay = LayoutInflater.from(this.getContext()).inflate(R.layout.list_events, parent, false);
+        initComponent(viewToDisplay);
+
+        Event event = getItem(position);
+
+        if (event != null)
+            setUIElements(event);
+
+        return viewToDisplay;
+    }
+
+    private void initComponent(View convertView) {
+        title = convertView.findViewById(R.id.eventTitle);
+        time = convertView.findViewById(R.id.time);
+        location = convertView.findViewById(R.id.location);
+        day = convertView.findViewById(R.id.day);
+        month = convertView.findViewById(R.id.month);
+    }
+
+    @Override
+    public Event getItem(int i) {
+        return events.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return events.size();
+    }
+
+    public void setUIElements(Event event) {
+
+    }
+}
--- app/src/main/java/com/ui/fitit/ui/ScheduleActivity.java
+++ app/src/main/java/com/ui/fitit/ui/ScheduleActivity.java
@@ -1,7 +1,14 @@
 package com.ui.fitit.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.ui.fitit.Adapters.EventAdapter;
 import com.ui.fitit.R;
 import com.ui.fitit.data.model.Event;
 import java.util.List;
@@ -9,6 +16,7 @@ import java.util.List;
 public class ScheduleActivity extends AppCompatActivity {
 
     List<Event> events;
+    EventAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +24,31 @@ public class ScheduleActivity extends AppCompatActivity {
         setContentView(R.layout.schedule_activity);
     }
 
-    private void initComponents() {
-
+    private void setEventsAdapter() {
+// Android adapter for list view
+        adapter = new RoutesAdapter(this, R.layout.list_routes, mViewModel.getRouteOptions());
+        allRoutes.setAdapter(adapter);
+        allRoutes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (carButton.isSelected() || transitButton.isSelected() || walkButton.isSelected()) {
+                    Intent openPaths = new Intent(RoutesActivity.this,
+                            PathsActivity.class);
+                    DirectionsRoute directionsResult = mViewModel.getDirectionsResult().routes[i];
+                    openPaths.putExtra("directionsResult", directionsResult);
+                    openPaths.putExtra("shuttle", false);
+                    startActivity(openPaths);
+                } else {
+                    if (mViewModel.getShuttles() == null) {
+                        Toast.makeText(getApplicationContext(), "Paths view for Shuttle route is not available if no shuttles exist.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Intent openPaths = new Intent(RoutesActivity.this,
+                                PathsActivity.class);
+                        openPaths.putExtra("shuttle", true);
+                        startActivity(openPaths);
+                    }
+                }
+            }
+        });
     }
 }
