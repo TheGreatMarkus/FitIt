980bed7e38440b36131f3a0b3c1c013056cc0e9b
Cristian Aldea <aldea.cristian@outlook.com>
2020-04-15 03:36:09 -0400

    Fix styling issue with feedback activity
    Fixed an issue where the stying of the feedback activity would slightly block things that weren't behind it. The code that was hardcoding the size of the activity was replaced with wrap_content and gravity center

--- app/src/main/java/com/ui/fitit/ui/FeedbackActivity.java
+++ app/src/main/java/com/ui/fitit/ui/FeedbackActivity.java
@@ -2,7 +2,6 @@ package com.ui.fitit.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -55,13 +54,6 @@ public class FeedbackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_feedback);
-
-        // Set the form to be 50% of the screen
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-        getWindow().setLayout((int) (width * 0.9), (int) (height * 0.5));
 
         initView();
         setupPersistentStorage();
